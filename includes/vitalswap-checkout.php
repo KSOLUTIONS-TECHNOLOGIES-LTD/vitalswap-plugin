be7fb2c7218eda02d5ad2d600f43e760ae61b647
kendam <kenny@ksolutionsng.com>
2025-03-27 20:15:57 +0100

    added checkout script

--- includes/vitalswap-checkout.php
+++ includes/vitalswap-checkout.php
@@ -1,38 +1,45 @@
 <html>
-    <head>
-        <script src="https://scripts.vitalswap.com/checkout.js" crossorigin="anonymous"></script>
 
-    </head>
-    
-    <body onload=loadVitalswap()>
-        <script>
-        let url = new URL(window.location);
-        console.log(window.location)
-const session_id  = url.searchParams.get("sId");
-const isOTP = url.searchParams.get("isOTP");
-const email  = url.searchParams.get("email");
-console.log(session_id);
-        function loadVitalswap(){
-            vitalswapCheckout.init({
+<head>
+	<script src="https://scripts.vitalswap.com/checkout.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body onload=loadVitalswap()>
+	<script>
+		let url = new URL(window.location);
+		
+		const session_id = url.searchParams.get("sId");
+		const isOTP = url.searchParams.get("isOTP");
+		const email = url.searchParams.get("email");
+		const returnUrl = url.searchParams.get("returnUrl");
+
+		function loadVitalswap() {
+			vitalswapCheckout.init({
 				session: session_id,
-				isOtp: false, 
-				email: email, 
-				// callback: "https://yourcheckoutpage.com", //Optional URL to redirect after completion
+				isOtp: false,
+				email: email,
+				callback: returnUrl,
 				environment: "sandbox", // Set to "sandbox" or "production"
 				onload: () => {
 					console.log("Checkout started");
 				},
 				onsuccess: (data) => {
-					console.log("Checkout successful:", data);
+					alert("Payment successful")
+					window.location.href = returnUrl
 				},
 				onclose: (data) => {
-					console.log("Checkout closed:", data);
+					window.location.href = returnUrl
+
 				},
 				onerror: (error) => {
 					console.error("Checkout error:", error);
+					alert("An error occurred, please try again")
+					
 				},
 			});
-        }
-        </script>
-    </body>
+		}
+	</script>
+</body>
+
 </html>
